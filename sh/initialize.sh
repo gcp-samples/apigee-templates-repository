a1769f77fdc2edc4718c4ed5b1d69fec7beb7aed
tyayers <tyler.ayers@gmail.com>
2026-08-20 12:56:10 +0200

    Update initialize.sh

--- sh/initialize.sh
+++ sh/initialize.sh
@@ -5,7 +5,7 @@ curl -X POST "https://apigee.googleapis.com/v1/organizations/$GOOGLE_CLOUD_PROJE
 }'
 curl -X POST "https://apigee.googleapis.com/v1/organizations/$GOOGLE_CLOUD_PROJECT/environments/$APIGEE_ENV/keyvaluemaps/AI-Config/entries" -H "Authorization: Bearer $(gcloud auth application-default print-access-token)" -H "Content-Type: application/json" -d '{
   "name": "ModelRoutingText",
-  "value": "{"models": {"google/": "googlecloud-oai", "anthropic/": "googlecloud", "openai/": "openai"}, "mappings": {"google/gemini-flash-latest": "google/gemini-3.6-flash"}}"
+  "value": "{\"models\": {\"google/\": \"googlecloud-oai\", \"anthropic/\": \"googlecloud\", \"openai/\": \"openai\"}, \"mappings\": {\"google/gemini-flash-latest\": \"google/gemini-3.6-flash\"}}"
 }'
 
 # data collectors
@@ -16,15 +16,15 @@ curl -X POST "https://apigee.googleapis.com/v1/organizations/$GOOGLE_CLOUD_PROJE
 curl -X POST "https://apigee.googleapis.com/v1/organizations/$GOOGLE_CLOUD_PROJECT/datacollectors" -H "Authorization: Bearer $(gcloud auth application-default print-access-token)" -H 'Content-Type: application/json; charset=utf-8' \
 -d '{"name": "dc_ai_cost_center", "description": "Cost center", "type": "STRING"}'
 curl -X POST "https://apigee.googleapis.com/v1/organizations/$GOOGLE_CLOUD_PROJECT/datacollectors" -H "Authorization: Bearer $(gcloud auth application-default print-access-token)" -H 'Content-Type: application/json; charset=utf-8' \
--d '{"name": "dc_ai_total_token_count", "description": "The total token count": "INTEGER"}'
+-d '{"name": "dc_ai_total_token_count", "description": "The total token count", "type": "INTEGER"}'
 curl -X POST "https://apigee.googleapis.com/v1/organizations/$GOOGLE_CLOUD_PROJECT/datacollectors" -H "Authorization: Bearer $(gcloud auth application-default print-access-token)" -H 'Content-Type: application/json; charset=utf-8' \
--d '{"name": "dc_ai_prompt_token_count", "description": "The prompt token count": "INTEGER"}'
+-d '{"name": "dc_ai_prompt_token_count", "description": "The prompt token count", "type": "INTEGER"}'
 curl -X POST "https://apigee.googleapis.com/v1/organizations/$GOOGLE_CLOUD_PROJECT/datacollectors" -H "Authorization: Bearer $(gcloud auth application-default print-access-token)" -H 'Content-Type: application/json; charset=utf-8' \
--d '{"name": "dc_ai_response_token_count", "description": "The response token count": "INTEGER"}'
+-d '{"name": "dc_ai_response_token_count", "description": "The response token count", "type": "INTEGER"}'
 curl -X POST "https://apigee.googleapis.com/v1/organizations/$GOOGLE_CLOUD_PROJECT/datacollectors" -H "Authorization: Bearer $(gcloud auth application-default print-access-token)" -H 'Content-Type: application/json; charset=utf-8' \
--d '{"name": "dc_ai_response_type", "description": "The response type, either streaming or non-streaming": "STRING"}'
+-d '{"name": "dc_ai_response_type", "description": "The response type, either streaming or non-streaming", "type": "STRING"}'
 curl -X POST "https://apigee.googleapis.com/v1/organizations/$GOOGLE_CLOUD_PROJECT/datacollectors" -H "Authorization: Bearer $(gcloud auth application-default print-access-token)" -H 'Content-Type: application/json; charset=utf-8' \
--d '{"name": "dc_ai_time_first_token", "description": "The time in milliseconds to the first token": "INTEGER"}'
+-d '{"name": "dc_ai_time_first_token", "description": "The time in milliseconds to the first token", "type": "INTEGER"}'
 curl -X POST "https://apigee.googleapis.com/v1/organizations/$GOOGLE_CLOUD_PROJECT/datacollectors" -H "Authorization: Bearer $(gcloud auth application-default print-access-token)" -H 'Content-Type: application/json; charset=utf-8' \
 -d '{"name": "dc_ai_request_cost", "description": "Request cost", "type": "FLOAT"}'
 curl -X POST "https://apigee.googleapis.com/v1/organizations/$GOOGLE_CLOUD_PROJECT/datacollectors" -H "Authorization: Bearer $(gcloud auth application-default print-access-token)" -H 'Content-Type: application/json; charset=utf-8' \
